e929928307de9153055b871cfdd4aef180591612
Charlie <charlie@relum.org>
2018-07-26 22:47:53 +0100

    [server] Updated login.php.
    Updated to allow for login requests from allowed useragents, check if username and hwid are matched to user in database then return response via output function.

--- login.php
+++ login.php
@@ -33,4 +33,34 @@
 	if ($ua != "Loader")
 		output(false, 'Wrong Client.');
 
+	// Set action variable
+	$action = $_GET['action'];
+	// Protect from SQL injection
+	$username = $conn->real_escape_string($_GET['username']);
+	$hwid = $conn->real_escape_string($_GET['hwid']);
+	// If logging in
+	if ($action == 'login') {
+		// Check that username + hwid have values
+		if (isset($_GET['username']) and isset($_GET['hwid']) and !empty($_GET['username']) and !empty($_GET['hwid'])) {
+			// Get username from request
+			$username = $_GET['username'];
+			// Select from the table users where username = post username & hwid = post hwid
+			$sql = "SELECT * FROM users WHERE uname='$username' and hwid='$hwid'";
+			// Database query
+			$res = mysqli_query($conn, $sql);
+			// Fetch associated from query
+			$row = mysqli_fetch_assoc($res);
+			// Check if user exists in database
+			if (mysqli_num_rows($res)<1) {
+				// User doesn't exist
+				output(false, 'Failed to verify user.');
+			} else {
+				// User exists
+        		output(true, 'Verified user successfully.');
+			}
+		} else {
+			// At least one request value is empty.
+			output(false, 'Failed to verify user.');
+		}
+	}
 ?>
