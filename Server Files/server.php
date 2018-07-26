c164ff660ea398955bf81a49ae9e784d5d1a1be1
Charlie <charlie@relum.org>
2018-07-26 22:36:30 +0100

    [server] Uploaded server.php.
    Uploaded server.php which sets mysqli connection.

---
+++ Server Files/server.php
@@ -0,0 +1,13 @@
+<?php
+
+	/*
+    
+        === Loader ===
+        Developed by Justin
+        Simple C# loader with PHP & MySQL integration.
+        Github: https://github.com/name/loader
+        
+    */
+
+	$conn = @mysqli_connect('Server Host','Username','Password','Database') or die('Couldn\'t connect to the database');
+?>
